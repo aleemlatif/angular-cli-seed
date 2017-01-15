551a08bb961417481c2ca0454c28879a464a35ef
A. Vatsaev <azero79@gmail.com>
2017-01-15 13:34:50 +0100

    added modal demo when submitting form in the footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
+import {MaterializeAction} from 'angular2-materialize';
 
 @Component({
   selector: 'app-footer',
@@ -7,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  modalActions = new EventEmitter<string|MaterializeAction>();
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  openModal() {
+    this.modalActions.emit({action:"modal",params:['open']});
+  }
+  closeModal() {
+    this.modalActions.emit({action:"modal",params:['close']});
   }
 
 }
